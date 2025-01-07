924442a70e440ac6b9be9239d9e6a8a5f564ce67
f3nrir77 <112421468+f3nrir77@users.noreply.github.com>
2025-01-07 00:09:25 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d''/)"
+set -ex
+
+# Grab the container's IP address
+IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')"
 
 cd /opt/cobaltstrike
 
-# Check if we already downloaded CS teamserver or do we grab a fresh copy
-FILE=/opt/cobaltstrike/cs-download-check
+# 1) Always download / update
+token=$(curl -s https://download.cobaltstrike.com/download \
+           -d "dlkey=${COBALTSTRIKE_KEY}" \
+       | grep 'href="/downloads/' | cut -d '/' -f3)
 
-if [ -f "$FILE" ]; then
-    /opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/${COBALTSTRIKE_PROFILE} ${COBALTSTRIKE_EXP}
-else
-	touch /opt/cobaltstrike/cs-download-check
-    token=`curl -s https://download.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
-	curl -s https://download.cobaltstrike.com/downloads/${token}/latest410/cobaltstrike-dist-linux.tgz -o /tmp/cobaltstrike.tgz
-	tar zxf /tmp/cobaltstrike.tgz -C /opt
-	echo ${COBALTSTRIKE_KEY} | /opt/cobaltstrike/update
-	/opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/${COBALTSTRIKE_PROFILE} ${COBALTSTRIKE_EXP}
-fi
+curl -s https://download.cobaltstrike.com/downloads/"${token}"/latest410/cobaltstrike-dist-linux.tgz \
+     -o /tmp/cobaltstrike.tgz
+
+tar zxf /tmp/cobaltstrike.tgz -C /opt
+
+# 2) Always update using the license key
+echo "${COBALTSTRIKE_KEY}" | /opt/cobaltstrike/update
+
+# 3) Finally run the teamserver
+/opt/cobaltstrike/teamserver \
+    "$IPADDRESS" \
+    "${COBALTSTRIKE_PASS}" \
+    "/opt/cobaltstrike/${COBALTSTRIKE_PROFILE}" \
+    "${COBALTSTRIKE_EXP}"
