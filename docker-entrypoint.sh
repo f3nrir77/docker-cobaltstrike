c2b81d8c33dc77f6748ce74d7332e0544f8401e2
f3nrir77 <112421468+f3nrir77@users.noreply.github.com>
2025-01-07 00:33:46 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,22 +7,33 @@ IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cu
 
 cd /opt/cobaltstrike
 
-# 1) Always download / update
-token=$(curl -s https://download.cobaltstrike.com/download \
-           -d "dlkey=${COBALTSTRIKE_KEY}" \
-       | grep 'href="/downloads/' | cut -d '/' -f3)
+# Check if we already downloaded CS teamserver or do we grab a fresh copy
+FILE=/opt/cobaltstrike/cs-download-check
 
-curl -s https://download.cobaltstrike.com/downloads/"${token}"/latest410/cobaltstrike-dist-linux.tgz \
-     -o /tmp/cobaltstrike.tgz
+if [ -f "$FILE" ]; then
+    /opt/cobaltstrike/server/teamserver \
+        "$IPADDRESS" \
+        "${COBALTSTRIKE_PASS}" \
+        "/opt/cobaltstrike/${COBALTSTRIKE_PROFILE}" \
+        "${COBALTSTRIKE_EXP}"
+else
+    # 1) Always download / update
+    token=$(curl -s https://download.cobaltstrike.com/download \
+                -d "dlkey=${COBALTSTRIKE_KEY}" \
+            | grep 'href="/downloads/' | cut -d '/' -f3)
 
-tar zxf /tmp/cobaltstrike.tgz -C /opt
+    curl -s https://download.cobaltstrike.com/downloads/"${token}"/latest410/cobaltstrike-dist-linux.tgz \
+         -o /tmp/cobaltstrike.tgz
 
-# 2) Always update using the license key
-echo "${COBALTSTRIKE_KEY}" | /opt/cobaltstrike/update
+    tar zxf /tmp/cobaltstrike.tgz -C /opt
 
-# 3) Finally run the teamserver
-/opt/cobaltstrike/server/teamserver \
-    "$IPADDRESS" \
-    "${COBALTSTRIKE_PASS}" \
-    "/opt/cobaltstrike/${COBALTSTRIKE_PROFILE}" \
-    "${COBALTSTRIKE_EXP}"
+    # 2) Always update using the license key
+    echo "${COBALTSTRIKE_KEY}" | /opt/cobaltstrike/update
+
+    # 3) Finally run the teamserver
+    /opt/cobaltstrike/server/teamserver \
+        "$IPADDRESS" \
+        "${COBALTSTRIKE_PASS}" \
+        "/opt/cobaltstrike/${COBALTSTRIKE_PROFILE}" \
+        "${COBALTSTRIKE_EXP}"
+fi
