45475bdb37f61cffccc78d364a593a71669d4607
f3nrir77 <112421468+f3nrir77@users.noreply.github.com>
2025-01-07 00:46:57 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,14 +11,19 @@ cd /opt/cobaltstrike
 FILE=/opt/cobaltstrike/cs-download-check
 
 if [ -f "$FILE" ]; then
-    /opt/cobaltstrike/server/teamserver \
+    # The important part here is that we cd into /opt/cobaltstrike/server 
+    # so the teamserver script finds its support files
+    cd /opt/cobaltstrike/server
+    ./teamserver \
         "$IPADDRESS" \
         "${COBALTSTRIKE_PASS}" \
         "/opt/cobaltstrike/${COBALTSTRIKE_PROFILE}" \
         "${COBALTSTRIKE_EXP}"
 else
-    # 1) Always download / update
+    # Mark that we've downloaded (and update) once
     touch /opt/cobaltstrike/cs-download-check
+
+    # 1) Always download / update
     token=$(curl -s https://download.cobaltstrike.com/download \
                 -d "dlkey=${COBALTSTRIKE_KEY}" \
             | grep 'href="/downloads/' | cut -d '/' -f3)
@@ -31,8 +36,9 @@ else
     # 2) Always update using the license key
     echo "${COBALTSTRIKE_KEY}" | /opt/cobaltstrike/update
 
-    # 3) Finally run the teamserver
-    /opt/cobaltstrike/server/teamserver \
+    # 3) Finally run the teamserver from its own directory
+    cd /opt/cobaltstrike/server
+    ./teamserver \
         "$IPADDRESS" \
         "${COBALTSTRIKE_PASS}" \
         "/opt/cobaltstrike/${COBALTSTRIKE_PROFILE}" \
